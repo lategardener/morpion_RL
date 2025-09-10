e7f66503b51b7a892976be9be6572eaf40b759a7
lategardener <djolemarc@cy-tech.fr>
2025-09-10 15:05:10 +0200

    post api : Env initialization

--- fast_api/api.py
+++ fast_api/api.py
@@ -2,6 +2,7 @@ import os
 from typing import Annotated
 
 from fastapi import FastAPI, Query, Path
+from pydantic import BaseModel
 
 from agents import *
 from envs.base_env import *
@@ -10,6 +11,11 @@ app = FastAPI()
 
 env: TicTacToeBaseEnv | None  = None
 agent: PPOAgent | RandomAgent | SmartRandomAgent | None = None
+
+class EnvConfigs(BaseModel):
+    board_length : int
+    pattern_victory_length : int
+    victory_reward : int
 
 
 @app.get("/")
@@ -23,22 +29,26 @@ async def observation():
         return None
     return env.get_observation()
 
+@app.post("/initEnv")
+async def init_env(configs: EnvConfigs):
+    global env
+    env = TicTacToeBaseEnv(board_length=configs.board_length,
+                           pattern_victory_length=configs.pattern_victory_length,
+                           victory_reward=configs.victory_reward
+                           )
+    return {
+        "message": "Success",
+        "board_length": env.board_length,
+        "pattern_victory_length": env.pattern_victory_length,
+        "victory_reward": env.victory_reward,
+        "player": env.player,
+        "gameboard": env.gameboard.tolist()  # numpy array -> liste
+    }
+
 @app.get("/agent/{board_length}/{win_pattern_length}/{version}/")
-async def get_agent(board_length: Annotated[
-                 int,
-                 Path(title="Path int",
-                       description="Board lines and columns number",
-                       ge=3)],
-                 win_pattern_length: Annotated[
-                     int,
-                     Path(title="Path int",
-                           description="Number of align pawns to win",
-                           ge=3)],
-                 version: Annotated[
-                     int,
-                     Path(title="Path int",
-                           description="Agent version",
-                           ge=1)]):
+async def get_agent(board_length: Annotated[int, Path(title="Path int", description="Board lines and columns number", ge=3)],
+                    win_pattern_length: Annotated[int, Path(title="Path int", description="Number of align pawns to win", ge=3)],
+                    version: Annotated[int,Path(title="Path int", description="Agent version", ge=1)]):
 
     agent = f"best_agents/agent_v{version}_{board_length}x{board_length}_{win_pattern_length}.zip"
     if os.path.exists(agent):

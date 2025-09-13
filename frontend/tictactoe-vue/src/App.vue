9847b46f2848407fced758fbec6598d798b8753a
lategardener <djolemarc@cy-tech.fr>
2025-09-13 21:15:21 +0200

    Frontend set up with vue.js

---
+++ frontend/tictactoe-vue/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+import Welcome from './Welcome.vue'
+
+const page = ref("Welcome")
+</script>
+
+<template>
+  <Welcome v-if="page === 'Welcome'"></Welcome>
+</template>
+
+<style scoped></style>
---
+++ frontend/tictactoe-vue/src/Welcome.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <h2>WELCOME TO TICTACTOE GAME</h2>
+  <button> START </button>
+</template>
+
+<style scoped>
+
+</style>

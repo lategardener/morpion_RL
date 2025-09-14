277a72d467cff1b4cf24be167171e347271fca1d
lategardener <djolemarc@cy-tech.fr>
2025-09-14 22:38:32 +0200

    retrieve available opponents ( Random, Smart Random, and so on)

--- fastapi_app/services/agent_service.py
+++ fastapi_app/services/agent_service.py
@@ -3,6 +3,7 @@ import os
 from fastapi import Path
 from agents import PPOAgent
 from typing import Annotated
+import re
 
 from fastapi_app.models.agent_model import GameModeConfigs
 
@@ -16,6 +17,27 @@ def init_game_mode(app, game_mode: GameModeConfigs) :
     return {
         "message": "Game mode initialized => Game mode : {}".format(game_mode.mode),
     }
+
+def get_available_opponents(app):
+    board_size = app.state.env.board_length
+    pattern_vl = app.state.env.pattern_victory_length
+    agents_dir = "best_agents"
+    if not os.path.exists(agents_dir):
+        raise AssertionError("Agent dir not found")
+
+    opponents = {
+        "Random": {},
+        "Smart Random": {},
+    }
+
+    pattern = rf"agent_v(\d+)_{board_size}x{board_size}_{pattern_vl}\.zip"
+    for agent_path in sorted(os.listdir(agents_dir)):
+        match = re.match(pattern, agent_path)
+        if match:
+            version = match.groups()[0]
+            opponents[f"AI agent version {version}"] = {"version": f"{version}"}
+
+    return opponents
 
 def get_agent(board_length: Annotated[int, Path(title="Path int", description="Board lines and columns number", ge=3)],
               win_pattern_length: Annotated[int, Path(title="Path int", description="Number of align pawns to win", ge=3)],

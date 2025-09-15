4505481a0b47b55076586e5378cacc737c823131
lategardener <djolemarc@cy-tech.fr>
2025-09-15 19:27:58 +0200

    Update route

--- fastapi_app/routers/game.py
+++ fastapi_app/routers/game.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter, Request
 from fastapi_app.models.agent_model import GameModeConfigs
 from fastapi_app.models.env_model import EnvConfigs, ActionPlayed
 from fastapi_app.services.agent_service import init_game_mode, get_available_opponents
-from fastapi_app.services.env_service import init_env, observation
+from fastapi_app.services.env_service import init_env, observation, action_played
 
 router = APIRouter()
 
@@ -16,7 +16,7 @@ def init_game_mode_route(request: Request, configs: GameModeConfigs):
     return init_game_mode(request.app, configs)
 
 @router.post("/actionPlayed")
-def action_played(request: Request, action: ActionPlayed):
+def action_played_(request: Request, action: ActionPlayed):
     return action_played(request.app, action)
 
 @router.get("/observation")

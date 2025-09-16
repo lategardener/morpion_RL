ee50843e5c4926b3058af8c2d68692cefc272bd2
lategardener <djolemarc@cy-tech.fr>
2025-09-16 13:11:59 +0200

    delete unused service

--- fastapi_app/services/agent_service.py
+++ fastapi_app/services/agent_service.py
@@ -50,11 +50,4 @@ def save_agent(app, agent_config:AgentConfigs):
         agent_path = f"best_agents/agent_v{agent['version']}_{env.board_length}x{env.board_length}_{env.victory_pattern_length}.zip"
         app.state.agent = PPOAgent(agent_path)
 
-def get_agent(board_length: Annotated[int, Path(title="Path int", description="Board lines and columns number", ge=3)],
-              win_pattern_length: Annotated[int, Path(title="Path int", description="Number of align pawns to win", ge=3)],
-              version: Annotated[int,Path(title="Path int", description="Agent version", ge=1)]):
 
-    agent = f"best_agents/agent_v{version}_{board_length}x{board_length}_{win_pattern_length}.zip"
-    if os.path.exists(agent):
-        return {"agent": PPOAgent(agent)}
-    return None
